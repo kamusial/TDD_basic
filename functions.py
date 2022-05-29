e4f26285c0b386a837a3667d34741aeb5373fe7e
kamusial <kamil.support@gmail.com>
2022-05-29 12:51:51 +0200

    gotowy fizzbuzz

--- functions.py
+++ functions.py
@@ -1,5 +1,13 @@
-def add(x, y):
-    return x + y
+def fizzbuzz(number):
+    if isinstance(number, int) and number >= 1:
+        if number%3 == 0 and number%5 == 0:
+            return 'fizzbuzz'
+        elif number%3 == 0:
+            return 'fizz'
+        elif number%5 == 0:
+            return 'buzz'
+        else:
+            return number
+    else:
+        return None
 
-def no_of_letters(x):
-    return len(x)
--- tests.py
+++ tests.py
@@ -1,7 +1,15 @@
 from functions import *
 
-def test_0():
-    assert add(5,3) == 8
-
 def test_1():
-    assert no_of_letters('Argument') == 8
+    assert fizzbuzz(1) == 1
+    assert fizzbuzz(2) == 2
+    assert fizzbuzz(3) == 'fizz'
+    assert fizzbuzz(5) == 'buzz'
+    assert fizzbuzz(6) == 'fizz'
+    assert fizzbuzz(10) == 'buzz'
+    assert fizzbuzz(15) == 'fizzbuzz'
+    assert fizzbuzz(0) == None
+    assert fizzbuzz(-5) == None
+    assert fizzbuzz('Mama') == None
+    assert fizzbuzz(1.7) == None
+    assert fizzbuzz('0') == None
